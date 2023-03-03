6f0464e34f8a8e849e3f356e1cb07c6df3e75ae0
David Smith <dws@dolcesfogato.com>
2023-03-03 11:41:05 -0800

    Set Swift to v5 and removed platforms.

--- Package.swift
+++ Package.swift
@@ -1,13 +1,10 @@
-// swift-tools-version:5.3
+// swift-tools-version:5.7
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "Chameleon",
-    platforms: [
-        .iOS(.v8)
-    ],
     products: [
         .library(
             name: "Chameleon",
@@ -23,15 +20,11 @@ let package = Package(
             name: "Chameleon",
             path:"Pod/Classes/Objective-C"
         ),
-//        .target(
-//            name: "Chameleons",
-//           // dependencies: ["Chameleon/Default"],
-//            path:"Chameleon", resources: [.process("Chameleon/Info.plist")]
-//        ),
         .target(
             name: "ChameleonSwift",
             dependencies: ["Chameleon"],
             path:"Pod/Classes/Swift"
         )
-    ]
+    ],
+    swiftLanguageVersions: [.v5]
 )
